faaf5444299cefcafad62a47163632529514cb58
Nate <nathanasare.1234@gmail.com>
2026-08-05 08:48:30 +0000

    Testing Users

--- gridcare/test_users.py
+++ gridcare/test_users.py
@@ -1,5 +1,18 @@
+"""
+test_users.py
+Seed test accounts for GridCare-Lite
+"""
+
 from login import add_user
 
-add_user('admin1', 'password321', 'admin')
-add_user('eng1', 'password246', 'engineer')
-add_user('tech1', 'password642', 'technician')
+
+def seed_test_users():
+    print("Creating test users...")
+    add_user('admin1', 'password321', 'admin')
+    add_user('eng1', 'password246', 'engineer')
+    add_user('tech1', 'password642', 'technician')
+    print("Test users created successfully!")
+
+
+if __name__ == '__main__':
+    seed_test_users()
